d00dc4b2faa9798c46eab90df30db9e2fec90832
Jyang042 <145074890+Jyang042@users.noreply.github.com>
2024-05-16 15:50:31 -0500

    UI Animations

---
+++ assets/smoothScroll.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+document.querySelectorAll('.navigation a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
